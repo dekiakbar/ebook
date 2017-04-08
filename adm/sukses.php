54a5d2bddbb529275c61138c7e035f6650b619f6
dekyAkbar <dekiakbar@linuxmail.org>
2017-04-08 23:58:25 +0700

    fix glyphicon mumet

--- adm/sukses.php
+++ adm/sukses.php
@@ -3,10 +3,12 @@
 	include ('../kon.php');
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<title>Admin Page</title>
+	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="../css/style.css">
 	<script type="text/javascript" href="../js/bootstrap.min.js"></script>
@@ -24,23 +26,31 @@
 						<th>Kategori</th>
 						<th>Tanggal</th>
 						<th>Donasi</th>
+						<th>Menu</th>
 					</tr>
 					<?php 
 					$sql = mysqli_query($koneksi,"SELECT * FROM buku ORDER BY jdl ASC");
 					if (mysqli_num_rows($sql) == 0) {
 							echo '<tr><td colspan="8">Data Masih Kosong.</td></tr>';
 					}else{
+						$no=1;
 						while($data = mysqli_fetch_assoc($sql)){
-							echo '
-							<tr>
-								<td>'.$data['id'].'</td>
-								<td><a href="#"'.$data['id'].'"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>'.$data['jdl'].'</a></td>
-	                            <td>'.$data['nmp'].'</td>
-	                            <td>'.$data['kat'].'</td>
-								<td>'.$data['tgl'].'</td>
-	                            <td><span class="label label-success">'.$data['donasi'].'</span></td>
-	                        </tr>';
-							
+						echo '
+						<tr>
+							<td>'.$no.'</td>
+							<td><a href="profile.php?nik="><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> '.$data['jdl'].'</a></td>
+                            <td>'.$data['nmp'].'</td>
+                            <td>'.$data['kat'].'</td>
+							<td>'.$data['tgl'].'</td>
+                            <td><span class="label label-info">'.$data['donasi'].'</span></td>
+							<td>
+								
+								<a href="edit.php?nik='.$data['id'].'" title="Edit Data" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>
+								<a href="index.php?aksi=delete&nik='.$data['id'].'" title="Hapus Data" onclick="return confirm(\'Anda yakin akan menghapus E-Book '.$data['jdl'].'?\')" class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
+							</td>
+						</tr>
+						';
+						$no++;
 						}
 					} 
 					?>
