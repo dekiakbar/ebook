1786637079a7eeacc1497aceae90c7a92fed5000
dekyAkbar <dekiakbar@linuxmail.org>
2017-04-09 00:01:14 +0700

    fix glyphicon mumet

---
+++ adm/edit.php
@@ -0,0 +1,24 @@
+<?php 
+include ('session.php');
+include ('kon.php');
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Admin Edit</title>
+ </head>
+ <body>
+ 
+ 	<div class="container">
+ 		<div class="content">
+ 			<H2>Edit Data E-Book</H2>
+
+ 			<?php 
+ 			
+ 			 ?>
+ 			
+ 		</div>
+ 	</div>
+ </body>
+ </html>
