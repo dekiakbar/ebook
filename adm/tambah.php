f6f08fb513ddcda0c5ebff24f36c8ac1ec9bd9c8
dekyAkbar <dekiakbar@linuxmail.org>
2017-04-13 00:00:17 +0700

    malah tambah parah

--- adm/tambah.php
+++ adm/tambah.php
@@ -16,75 +16,81 @@
 	<script type="text/javascript" src="../js/sweetalert2.min.js"></script>
 	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="../js/jquery.js"></script>
+	<style type="text/css">
+		body{
+		
+		}
+	</style>
 </head>
 <body>
-	
-	<div class="container">
-		<div class="content">
-			<div class="row">
-				<div class="col-sm-12">
-					<form class="form-horizontal col-sm-6" action="" method="post" enctype="multipart/form-data">
-						<h2 class="text-center">Form Insert E-Book</h2>
-						<br>
-						<div class="form-group">
-							<label class="control-label col-sm-4">Judul Buku:</label>
-							<div class="col-sm-8">
-								<input type="text" name="judul" class="form-control" placeholder="Masukan Judul Buku">
+	<div class="gambar"></div>
+	<div class="clear"></div>	
+		<div class="container tengah">
+			<div class="content">
+				<div class="row">
+					<div class="col-sm-12">
+						<form class="form-horizontal col-sm-6" action="" method="post" enctype="multipart/form-data">
+							<h2 class="text-center">Form Insert E-Book</h2>
+							<br>
+							<div class="form-group">
+								<label class="control-label col-sm-4">Judul Buku:</label>
+								<div class="col-sm-8">
+									<input type="text" name="judul" class="form-control" placeholder="Masukan Judul Buku" required>
+								</div>
 							</div>
-						</div>
-						<div class="form-group">
-							<label class="control-label col-sm-4">Nama Pengarang:</label>
-							<div class="col-sm-8">
-								<input type="text" name="pengarang" class="form-control" placeholder="Masukan Nama Pengarang">
+							<div class="form-group">
+								<label class="control-label col-sm-4">Nama Pengarang:</label>
+								<div class="col-sm-8">
+									<input type="text" name="pengarang" class="form-control" placeholder="Masukan Nama Pengarang" required>
+								</div>
 							</div>
-						</div>
-						<div class="form-group">
-							<label class="control-label col-sm-4">Donasi:</label>
-							<div class="col-sm-8">
-								<input type="text" name="donasi" class="form-control" placeholder="Nama Penyumbang Buku">
+							<div class="form-group">
+								<label class="control-label col-sm-4">Donasi:</label>
+								<div class="col-sm-8">
+									<input type="text" name="donasi" class="form-control" placeholder="Nama Penyumbang Buku" required>
+								</div>
 							</div>
-						</div>
-						<div class="form-group">
-							<label class="control-label col-sm-4">File:</label>
-							<div class="col-sm-6">
-								<input class="file" type="file" name="file">
+							<div class="form-group">
+								<label class="control-label col-sm-4">File:</label>
+								<div class="col-sm-6">
+									<input class="file" type="file" name="file" required>
+								</div>
 							</div>
-						</div>
-						<div class="form-group">
-							<label class="control-label col-sm-4">Kategori:</label>
-							<div class="col-sm-8 text-left">
-								<?php 
-								$dbkate	= mysqli_query($koneksi,"SELECT * FROM kate");
-									if (mysqli_num_rows($dbkate) == 0) {
-										echo "<script>swal('Error','Belum ada kategori','error');</script>
-										<h4>Kategori Kosong</h4>";
-									}else{
-										while ($data = mysqli_fetch_assoc($dbkate)) {
-											echo '
-												<label class="checkbox-inline">
-													<input type="checkbox" name="kat[]" value="'.$data['id'].'"><span class="label label-success">'.$data['kategori'].'</span>
-												</label>
-											';
+							<div class="form-group">
+								<label class="control-label col-sm-4">Kategori:</label>
+								<div class="col-sm-8 text-center">
+									<?php 
+									$dbkate	= mysqli_query($koneksi,"SELECT * FROM kate");
+										if (mysqli_num_rows($dbkate) == 0) {
+											echo "<script>swal('Error','Belum ada kategori','error');</script>
+											<h4>Kategori Kosong</h4>";
+										}else{
+											while ($data = mysqli_fetch_assoc($dbkate)) {
+												echo '
+													<label class="checkbox-inline">
+														<input type="checkbox" name="kat[]" value="'.$data['id'].'"><span class="label label-info">'.$data['kategori'].'</span>
+													</label>
+												';
+											}
 										}
-									}
-								 ?>
+									 ?>
+								</div>
 							</div>
-						</div>
-						<div class="form-group">
-							<label class="control-label col-sm-4">Password</label>
-							<div class="col-sm-8">
-								<input type="password" name="pass" class="form-control" placeholder="Password">
+							<div class="form-group">
+								<label class="control-label col-sm-4">Password</label>
+								<div class="col-sm-8">
+									<input type="password" name="pass" class="form-control" placeholder="Password" required>
+								</div>
 							</div>
-						</div>
-						<div class="col-sm-12 text-center">
-							<input type="submit" name="tambah" value="Tambah" class="btn btn-primary btn-sm">
-						</div>
-					</form>
+							<div class="col-sm-12 text-center">
+								<input type="submit" name="tambah" value="Tambah" class="btn btn-primary btn-sm">
+							</div>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-
+		</div>
 	<?php 
 	if (isset($_POST['tambah'])) {
 			$nama 		= $_FILES['file']['name'];
@@ -92,21 +98,21 @@
 			$extensi	= strtolower(end($tipe));
 			$jenis 		= array('doc','docx','pdf');
 			$ukuran		= $_FILES['file']['size'];
+			$file 		= $_FILES['file']['tmp_name'];
 			$judul 		= $_POST['judul'];
 			$pengarang 	= $_POST['pengarang'];
 			$donasi 	= $_POST['donasi'];
-			$kat 		= $_POST['kat'];
 			$pass 		= md5($_POST['pass']);
 			$tgl		= date('h:i:s j-m-y');
-			$file 	= $_FILES['file']['tmp_name'];
-			
+			$kat      	= implode(',', $_POST['kat']);
+
 			$tes 		= mysqli_query($koneksi," SELECT * FROM buku WHERE jdl='$judul'");
 			$apass		= mysqli_query($koneksi," SELECT * FROM admin WHERE pass='$pass'");
 			if (mysqli_num_rows($tes) == 0) {
 				if (mysqli_num_rows($apass) == 1) {
 					if (in_array($extensi, $jenis) === true) {
 						move_uploaded_file($file, '../ebook/'.$nama);
-						$insert = mysqli_query($koneksi," INSERT INTO buku(jdl, nmp, kat, link, tgl, donasi, ukuran)VALUES ('$judul', '$pengarang', '$kat', '../ebook/'.'$nama', '$tgl', '$donasi', '$ukuran')") or die(mysqli_error());
+						$insert = mysqli_query($koneksi," INSERT INTO buku(jdl, nmp, kat, link, tgl, donasi, ukuran)VALUES ('$judul', '$pengarang', '$kat', '../ebook/$nama', '$tgl', '$donasi', '$ukuran')") or die('Error: ' . mysqli_error($koneksi));
 						if ($insert) {
 							echo "<script>swal('Sukses','Upload Berhasil','success');</script>";
 						}else{
