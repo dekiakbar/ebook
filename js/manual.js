dd59bb6b1049412b034b4bfbfb2cfba79e855211
dekyAkbar <dekiakbar@linuxmail.org>
2017-04-18 22:27:53 +0700

    Js dan konfir delet

---
+++ js/manual.js
@@ -0,0 +1,12 @@
+function konfirdel(){
+	swal({
+  		title: 'Are you sure?',
+  		text: "You won't be able to revert this!",
+  		type: 'warning',
+  		showCancelButton: true,
+  		confirmButtonColor: '#3085d6',
+  		cancelButtonColor: '#d33',
+  		confirmButtonText: 'Yes, delete it!'
+	}).then(function () {
+		})
+}
