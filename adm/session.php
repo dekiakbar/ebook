caca38709a19e34f028df92295ea42968dcab830
dekyAkbar <dekiakbar@linuxmail.org>
2017-04-25 03:47:40 +0700

    lmyan

--- adm/session.php
+++ adm/session.php
@@ -1,9 +1,14 @@
 <?php 
 include ("../kon.php");
 session_start();
-   if(isset($_SESSION['user'])){
-   		echo($_SESSION['user']);
-   		echo($_SESSION['login']);
+   if(isset($_SESSION['user'],$_SESSION['login'])){
+   	$user	= $_SESSION['user'];
+   	$login	= $_SESSION['login'];
+   	$pasdb	= mysqli_query($koneksi, "SELECT pass FROM admin WHERE user='$user'");
+   		if (mysqli_num_rows($pasdb) == 1) {
+   			$data = mysqli_fetch_assoc($pasdb);
+   			echo $data['pass'];
+   		}
    }else{
 		header("Location: adm.php");
    }
