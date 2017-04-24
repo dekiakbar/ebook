ee8ee4fd2b62cfe503593204a7de606716257c47
dekyAkbar <dekiakbar@linuxmail.org>
2017-04-24 22:26:12 +0700

    fix delet session logot

--- adm/logout.php
+++ adm/logout.php
@@ -1,7 +1,17 @@
 <?php
-   session_start();
-   
-   if(session_destroy()) {
-      header("Location: adm.php");
-   }
+include 'session.php';
+
+session();
+$_SESSION	= array();
+$isi		= session_get_cookie_params();
+
+setcookie(
+	session_name(),'',
+	time() - 42000,
+	$isi['path'],
+	$isi['domain'],
+	$isi['stopjs'],
+	$isi['http']);
+session_destroy();
+header("Location: adm.php");
 ?>
