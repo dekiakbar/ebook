3d015d21bc8a516d071e6eab8efacd2bc759eeed
dekyAkbar <dekiakbar@linuxmail.org>
2017-04-24 22:12:41 +0700

    fix session

--- adm/session.php
+++ adm/session.php
@@ -1,7 +1,26 @@
 <?php 
 include ("../kon.php");
-session_start();
-   if(!isset($_SESSION['login_user'])){
-		header("Location: adm.php");
-   }
- ?>
+
+function session(){
+	$namses	='sessionid';
+	session_name($namses);
+	$stopjs	=true;
+	$http	=true;
+
+	if (ini_set('session.use_only_cookies', 1) === false) {
+		header("Location: ../error.php?err=Could not initiate a safe session (ini_set)");
+        exit();
+    }
+
+    $cookie 	= session_get_cookie_params();
+    session_set_cookie_params(
+    	$cookie["lifetime"],
+    	$cookie["path"],
+    	$cookie["domain"],
+    	$stopjs,
+    	$http);
+
+    session_start();
+    session_regenerate_id(true);
+}
+ ?>}
