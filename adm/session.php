250b3112916a7c9f0ee76001d9b048e3e14e7e01
dekyAkbar <dekiakbar@linuxmail.org>
2017-04-25 03:58:20 +0700

    akhirnya jalan

--- adm/session.php
+++ adm/session.php
@@ -1,15 +1,24 @@
 <?php 
 include ("../kon.php");
 session_start();
-   if(isset($_SESSION['user'],$_SESSION['login'])){
+    if(isset($_SESSION['user'],$_SESSION['login'])){
    	$user	= $_SESSION['user'];
    	$login	= $_SESSION['login'];
    	$pasdb	= mysqli_query($koneksi, "SELECT pass FROM admin WHERE user='$user'");
    		if (mysqli_num_rows($pasdb) == 1) {
-   			$data = mysqli_fetch_assoc($pasdb);
-   			echo $data['pass'];
+   			$browser= $_SERVER['HTTP_USER_AGENT']; 
+   			$data	= mysqli_fetch_assoc($pasdb);
+   			$enpas	= hash('sha512', $browser.$data['pass']);
+   			
+   			if (hash_equals($login,$enpas)) {
+   			
+   			}else{
+   				header("Location: adm.php");
+   			}
+   		}else{
+   			header("Location: adm.php");
    		}
-   }else{
+    }else{
 		header("Location: adm.php");
    }
  ?>
