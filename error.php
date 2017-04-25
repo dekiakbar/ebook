cd3b13753f3f3f3f656366b1d32ece4281a861fa
dekyAkbar <dekiakbar@linuxmail.org>
2017-04-25 23:32:47 +0700

    ktinggalan he

---
+++ error.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Error Gan!</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/sweetalert2.css">
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/sweetalert2.min.js"></script>
+    <link rel="shortcut icon" href="gambar/ssd.ico">
+</head>
+<body class="bodi4">
+	<div class="container">
+		<div class="content">
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2 tengh warbel">
+					<div class="row">
+						<div class="col-sm-12 text-center">
+							<h2>Error Gan</h2>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-12 text-center">
+							<span class="eror">4<img src="gambar/emot.gif">4</span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-12">
+							<h5>swjqkjkqwjdkqjwdqjwkldqjwdkj</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
