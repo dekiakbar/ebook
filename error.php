b99d788d70355f02c5aac3a6166d25af30b08121
dekyAkbar <dekiakbar@linuxmail.org>
2017-04-25 23:55:09 +0700

    update layout eror

--- error.php
+++ error.php
@@ -7,30 +7,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link rel="stylesheet" type="text/css" href="css/sweetalert2.css">
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/sweetalert2.min.js"></script>
     <link rel="shortcut icon" href="gambar/ssd.ico">
 </head>
 <body class="bodi4">
 	<div class="container">
 		<div class="content">
 			<div class="row">
-				<div class="col-sm-8 col-sm-offset-2 tengh warbel">
+				<div class="col-sm-6 tengh warbel">
 					<div class="row">
 						<div class="col-sm-12 text-center">
-							<h2>Error Gan</h2>
+							<h2>Eror Gan!</h2>
 						</div>
-					</div>
-					<div class="row">
 						<div class="col-sm-12 text-center">
 							<span class="eror">4<img src="gambar/emot.gif">4</span>
 						</div>
-					</div>
-					<div class="row">
-						<div class="col-sm-12">
-							<h5>swjqkjkqwjdkqjwdqjwkldqjwdkj</h5>
+						<div class="col-sm-12 text-center">
+							<p>Halaman yang ente cari kgk ada</p>
+						</div>
+						<div class="row">
+							<div class="col-sm-12 tmbol text-center">
+							<a href="#">Home</a>
+							</div>	
 						</div>
 					</div>
 				</div>
