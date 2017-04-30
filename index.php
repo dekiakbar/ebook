11509750f4210e8756467d96b5c2759b170db721
dekyAkbar <dekiakbar@linuxmail.org>
2017-04-30 18:19:27 +0700

    fix link css,js,gambar

--- index.php
+++ index.php
@@ -3,10 +3,15 @@
 <head>
   <title>LEARNING E-BOOK</title>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <script src="js/jquery.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/sweetalert2.css">
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/sweetalert2.min.js"></script>
+  <link rel="shortcut icon" href="gambar/ssd.ico">
   <style>
     .navbar {
       margin-bottom: 50px;
