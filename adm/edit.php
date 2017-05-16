aa6a27b8d64013c522b97513150b72b39722fabe
dekyAkbar <dekiakbar@linuxmail.org>
2017-05-17 00:16:45 +0700

    fix edit page

--- adm/edit.php
+++ adm/edit.php
@@ -5,9 +5,12 @@ require_once '../kripto.php';
  
  	if (isset($_GET['nib'])) {
 		$id 		= encryptor('decrypt',$_GET['nib']);
-		$ambilbuku 	= mysqli_query($koneksi,"SELECT * FROM buku WHERE id='$id'");
-		$datadb = mysqli_fetch_assoc($ambilbuku);
+	}else{
+		header('Location : sukses.php');
 	}
+
+	$ambilbuku 	= mysqli_query($koneksi,"SELECT * FROM buku WHERE id='$id'");
+	$datadb = mysqli_fetch_assoc($ambilbuku);
 ?>
 
  <!DOCTYPE html>
@@ -72,12 +75,6 @@ require_once '../kripto.php';
 						<label class="control-label col-sm-4">Donasi:</label>
 						<div class="col-sm-8">
 							<input type="text" name="donasi" class="form-control" placeholder="Nama Penyumbang Buku" required>
-						</div>
-					</div>
-					<div class="form-group">
-						<label class="control-label col-sm-4">File:</label>
-						<div class="col-sm-2">
-							<input class="file" type="file" name="file" required>
 						</div>
 					</div>
 					<div class="form-group">
@@ -174,38 +171,26 @@ require_once '../kripto.php';
 
 <?php 
 	if (isset($_POST['apdet'])) {
-				$nama 		= $_FILES['file']['name'];
-				$tipe		= explode('.',$_FILES['file']['name']);
-				$extensi	= strtolower(end($tipe));
-				$jenis 		= array('doc','docx','pdf');
-				$ukuran		= $_FILES['file']['size'];
-				$file 		= $_FILES['file']['tmp_name'];
 				$judul 		= $_POST['judul'];
 				$pengarang 	= $_POST['pengarang'];
 				$donasi 	= $_POST['donasi'];
 				$pass 		= md5($_POST['pass']);
-				$tgl		= date('h:i:s j-m-y');
+				$tgl		= date("Y-m-d H:i:s");
 				$kat      	= implode(',', $_POST['kat']);
 
 				$tes 		= mysqli_query($koneksi," SELECT * FROM buku WHERE jdl='$judul'");
 				$apass		= mysqli_query($koneksi," SELECT * FROM admin WHERE pass='$pass'");
 				if (mysqli_num_rows($tes) == 0) {
 					if (mysqli_num_rows($apass) == 1) {
-						if (in_array($extensi, $jenis) === true) {
-							move_uploaded_file($file, '../ebook/'.$nama);
-							$insert = mysqli_query($koneksi," INSERT INTO buku(jdl, nmp, kat, link, tgl, donasi, ukuran)VALUES ('$judul', '$pengarang', '$kat', '../ebook/$nama', '$tgl', '$donasi', '$ukuran')") or die('Error: ' . mysqli_error($koneksi));
+							$insert = mysqli_query($koneksi,"UPDATE buku SET jdl='$judul', nmp='$pengarang', kat='$kat', link=link, tgl='$tgl', donasi='$donasi', ukuran=ukuran WHERE id='$id'") or die('Error: ' . mysqli_error($koneksi));
 							if ($insert) {
-								echo "<script>swal('Berhasil','Upload Berhasil','success');</script>";
+								echo "<script>swal('Berhasil','Data Berhasil Diperbaharui','success');</script>";
 							}else{
-								echo "<script>swal('Gagal','Upload Gagal','error');</script>";
+								echo "<script>swal('Gagal','Data Gagal Diperbaharui','error');</script>";
 							}
 						}else{
 							echo "<script>swal('Error','File yang diperbolehkan .pdf, .doc, .docx','error');</script>";
 						}
-							
-					}else{
-						echo "<script>swal('Error','Password Salah!','error');</script>";
-					}
 				}else{
 					echo "<script>swal('Error','Buku Sudah Ada','error');</script>";
 				}
